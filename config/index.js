2a4b0468b7aa06f0458c45549898afa8fff3ae7c
Fernando Braz <fernandob@thisismn.com>
2016-07-06 10:40:13 +0100

    Update mongodb to an external one and declare node and npm versions

--- config/index.js
+++ config/index.js
@@ -1,11 +1,11 @@
-// var configValues = require('./config');
-var mongoose = require('mongoose');
-console.log("Connection to DB...");
-var db = mongoose.connect('mongodb://localhost/holiday-booking');
-module.exports = db;
+var configValues = require('./config');
+// var mongoose = require('mongoose');
+// console.log("Connection to DB...");
+// var db = mongoose.connect('mongodb://localhost/holiday-booking');
+// module.exports = db;
 
-// module.exports = {
-//     getDbConnectionString: function(){
-//         // return 'mongodb://' + configValues.uname + ':' + configValues.pwd + '@ds017852.mlab.com:17852/holiday-booking';
-//     }
-// }
+module.exports = {
+    getDbConnectionString: function(){
+        return 'mongodb://' + configValues.uname + ':' + configValues.pwd + '@ds017852.mlab.com:17852/holiday-booking';
+    }
+}
