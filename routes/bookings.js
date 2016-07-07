90ce5cd9461f756b456013d6aa48c5a3508f1c7b
Fernando Braz <fernandob@thisismn.com>
2016-07-07 10:38:58 +0100

    Add functionality for undo approvals/rejection

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,20 +1,24 @@
 "use strict";
-var express = require('express');
-var router = express.Router();
-var moment = require('moment');
-var bodyParser = require('body-parser')
-var Booking = require('../models/booking');
-var User = require('../models/user');
+var express         = require('express');
+var router          = express.Router();
+var moment          = require('moment');
+var bodyParser      = require('body-parser')
+var Booking         = require('../models/booking');
+var User            = require('../models/user');
 var BookingApproval = require('../models/bookingApproval');
-var today = moment();
+var authorisations  = require('../helpers/authorisations');
+var today           = moment();
 
-/* GET home page. */
 
-router.get('/new', isLoggedIn, function(req, res, next) {
+// =========================================================
+// =============== New and Create Booking ==================
+// =========================================================
+
+router.get('/new', authorisations.isLoggedIn, function(req, res, next) {
   res.render('bookings/new.ejs', { today: today, currentUser : req.user });
 });
 
-router.post('/register', isLoggedIn, function(req, res){
+router.post('/register', authorisations.isLoggedIn, function(req, res){
   var currentUser = req.user;
   var userId = req.body.userId;
   var name = req.body.name;
@@ -27,13 +31,13 @@ router.post('/register', isLoggedIn, function(req, res){
   var status = "pending";
   
   if(startDate === "" || endDate === "") {
-    message = "You forgot some fields! Try again.";
+    var message = "You forgot some fields! Try again.";
     res.render('bookings/new.ejs', { today: today, currentUser : req.user, message: message, name: name });
   }else if(moment(startDate).diff(moment(endDate), 'days') > 0 || moment(startDate).diff(moment(dateOfRequest)) < 0){
-    message = "The dates selected are invalid! Try again.";
+    var message = "The dates selected are invalid! Try again.";
     res.render('bookings/new.ejs', { today: today, currentUser : req.user, message: message, name: name });
   }else if(parseInt(currentUser.holidaysLeft) - parseInt(totalDays) < 0){
-    message = "You do not have that many holidays left.";
+    var message = "You do not have that many holidays left.";
     res.render('bookings/new.ejs', { today: today, currentUser : req.user, message: message, name: name });
   }else{
     var newBooking = Booking();
@@ -49,19 +53,54 @@ router.post('/register', isLoggedIn, function(req, res){
     newBooking.save(function(err){
       
       if(err) throw err;
-      console.log(res.req);
-      res.sendStatus(200);
       res.redirect("/bookings/personal_requests");
     });
   }
 });
 
-router.get('/personal_requests', isLoggedIn, function(req, res){
+// =========================================================
+// ================== Delete Booking =======================
+// =========================================================
+
+router.get('/delete/:id', authorisations.isLoggedIn, function(req, res){
+    Booking.findById(req.params.id, function(err, booking){
+      if(err){
+        res.status(err.status || 500);
+        res.render('error', {
+          message: err.message,
+          error: err
+        });
+      }
+
+      if(booking.status === "pending"){
+        booking.remove({}, function(err){
+          if (err) {
+              res.statusCode = 403;
+              res.send(err);
+          } else {
+            res.location("personal_requests");
+            res.redirect("/bookings/personal_requests");
+          }
+        });
+      }else{
+        res.status(err.status || 500);
+        res.render('error', {
+          message: "You are trying to remove a record via a suspicius action.",
+          error: "Unauthorised Action"
+        });
+      }
+    });
+  });
+
+// =========================================================
+// ================== Personal Bookings ====================
+// =========================================================
+
+router.get('/personal_requests', authorisations.isLoggedIn, function(req, res){
   var currentUser = req.user;
   Booking.find({userId: currentUser._id}, function(err, bookings) {
 			if(err) throw err;
 				
-        console.log(res.req.body);
   			res.render('bookings/personalRequests.ejs', {
   				currentUser: currentUser,
           bookings: bookings,
@@ -71,7 +110,11 @@ router.get('/personal_requests', isLoggedIn, function(req, res){
 			});
 });
 
-router.get('/requests', isLoggedIn, isAdmin, function(req, res){
+// =========================================================
+// ============== Pending Bookings Requests ================
+// =========================================================
+
+router.get('/requests', authorisations.isLoggedIn, authorisations.isAdmin, function(req, res){
   Booking.find({ status: "pending" }, function(err, bookings) {
     if(err) throw err;
     
@@ -82,7 +125,12 @@ router.get('/requests', isLoggedIn, isAdmin, function(req, res){
     });
   });
 });
-router.get('/history', isLoggedIn, isAdmin, function(req, res){
+
+// =========================================================
+// ============== History of Booking Requests ==============
+// =========================================================
+
+router.get('/history', authorisations.isLoggedIn, authorisations.isAdmin, function(req, res){
   Booking.find({ status: { $in: ["accepted", "rejected"] } }, function(err, bookings) {
     if(err) throw err;
     
@@ -93,7 +141,12 @@ router.get('/history', isLoggedIn, isAdmin, function(req, res){
     });
   });
 });
-router.post('/approval/:approval_num', isLoggedIn, isAdmin, function(req, res){
+
+// =========================================================
+// ============== Approvals & Rejections ===================
+// =========================================================
+
+router.post('/approval/:approval_num', authorisations.isLoggedIn, authorisations.isAdmin, function(req, res){
   var currentUser = req.user;
   var bookingId = req.body.id;
   var approvalNum = req.params.approval_num;
@@ -136,7 +189,8 @@ router.post('/approval/:approval_num', isLoggedIn, isAdmin, function(req, res){
     });
   });
 });
-router.post('/reject/:approval_num', isLoggedIn, isAdmin, function(req, res){
+
+router.post('/reject/:approval_num', authorisations.isLoggedIn, authorisations.isAdmin, function(req, res){
   var currentUser = req.user;
   var bookingId = req.body.id;
   var approvalNum = req.params.approval_num;
@@ -161,7 +215,8 @@ router.post('/reject/:approval_num', isLoggedIn, isAdmin, function(req, res){
     });
   });
 });
-router.get('/decision/:id/:approval_num', isLoggedIn, function(req, res){
+
+router.get('/decision/:id/:approval_num', authorisations.isLoggedIn, function(req, res){
   var bookingId = req.params.id;
   var approvalNum = req.params.approval_num;
   if(approvalNum === "0"){
@@ -183,35 +238,41 @@ router.get('/decision/:id/:approval_num', isLoggedIn, function(req, res){
     });
   });
 });
-router.get("/undo/:id", isLoggedIn, isAdmin, function(req, res){
+
+router.get("/undo/:id", authorisations.isLoggedIn, authorisations.isAdmin, function(req, res){
   var bookingId = req.params.id;
   Booking.findById(bookingId, function(err, booking){
     if(err) throw err;
-    // change the status to pending
-    // set both approvals to false
-    if(booking.status === "accepted"){
-      // give back the days left to the user
-    }else if(booking.status === "rejected"){
-      
-    }
+    var currentStatus = booking.status;
+    booking.status = "pending";
+    booking.authorisation1 = false;
+    booking.authorisation2 = false;
+
+    booking.save(function(err){
+      if(err) throw err;
+
+      BookingApproval.find({bookingId: booking._id}, function(err, apprBookings){
+        if(err) throw err;
+
+        apprBookings.forEach(function(apprbooking){
+          apprbooking.remove({},function(){ console.log("deleting approval booking record...") });
+        });
+      });
+      if(currentStatus === "accepted"){
+        User.findById(booking.userId, function(err, user){
+          if(err) throw err;
+
+          user.holidaysLeft = parseInt(user.holidaysLeft) + parseInt(booking.totalDays) + "";
+          user.save(function(err){
+            if(err) throw err;
+
+            console.log("user's holidays restored.");
+          });
+        });
+      }
+      res.redirect("/bookings/history");
+    });
   });
 });
-function isAdmin(req, res, next) {
 
-    // if user is authenticated in the session, carry on 
-    if (req.user.admin)
-        return next();
-
-    // if they aren't redirect them to the home page
-    res.redirect('/');
-}
-function isLoggedIn(req, res, next) {
-
-    // if user is authenticated in the session, carry on 
-    if (req.isAuthenticated())
-        return next();
-
-    // if they aren't redirect them to the home page
-    res.redirect('/users/login');
-}
 module.exports = router;
